a8f5ec577ee1b195aa5603c94db7b47d94cbda08
Nikolay Skurkov <ntwdconst@gmail.com>
2023-02-14 14:47:17 +0700

    added a demonstration of the work

--- HomeWork5/Program.cs
+++ HomeWork5/Program.cs
@@ -1,7 +1,16 @@
-﻿class Program
+﻿using HomeWork5;
+
+class Program
 {
     static void Main()
     {
+        var q = new Quadcopter();
+        q.Charge();
+        IFlyingRobot flyingRobot = q;
+        Console.WriteLine(flyingRobot.GetRobotType());
+        IRobot robot = q;
+        Console.WriteLine(robot.GetRobotType());
+
 
     }
 }
